f351d3f3d1025ba50d83d57a53d0f1cc795466a3
binghan <binghan60@gmail.com>
2022-07-12 19:31:28 +0800

    Footer complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import Nav from './components/Nav'
-import Footer from './components/Footer'
+import React from "react";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Index from "./pages/Index";
 
 const App = () => {
   return (
     <>
       <Nav />
+      <Index />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,13 +3,15 @@ import React from "react";
 const Nav = () => {
   return (
     <nav className="d-flex justify-content-between align-items-center">
-      <div>LOGO</div>
+      <a href="/">
+        <img src="../images/nav_logo.png" />
+      </a>
       <ul className="d-flex">
         <li className="mx-4">關於我們</li>
         <li className="mx-4">最新消息</li>
         <li className="mx-4">商品一覽</li>
         <li className="mx-4">
-          <i class="fa-solid fa-user"></i>s{" "}
+          <i class="fa-solid fa-user"></i>
         </li>
         <li className="mx-4">
           <i class="fa-solid fa-cart-shopping"></i>
