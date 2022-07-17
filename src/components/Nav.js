b2559204bf49ea1942d2cec078d85bcbde8dd5af
binghan <binghan60@gmail.com>
2022-07-17 18:45:48 +0800

    修改alt

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,17 +4,17 @@ const Nav = () => {
   return (
     <nav className="d-flex justify-content-between align-items-center">
       <a href="/">
-        <img src="../images/nav_logo.png" />
+        <img src="../images/nav_logo.png" alt=""/>
       </a>
       <ul className="d-flex">
         <li className="mx-4">關於我們</li>
         <li className="mx-4">最新消息</li>
         <li className="mx-4">商品一覽</li>
         <li className="mx-4">
-          <i class="fa-solid fa-user"></i>
+          <i className="fa-solid fa-user"></i>
         </li>
         <li className="mx-4">
-          <i class="fa-solid fa-cart-shopping"></i>
+          <i className="fa-solid fa-cart-shopping"></i>
         </li>
       </ul>
     </nav>
