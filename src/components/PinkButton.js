70cf52e0220d0e0e81a93ffb16f1238f9ade4f45
binghan <binghan60@gmail.com>
2022-07-17 21:18:27 +0800

    建立Pink_Buttom

---
+++ src/components/PinkButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PinkButton = () => {
+  return (
+    <>
+      <button className="pink_button">LEARN MORE</button>
+    </>
+  );
+};
+
+export default PinkButton;
